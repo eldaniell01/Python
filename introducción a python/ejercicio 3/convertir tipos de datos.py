eb92402b16dd5bcd9b66bd29e4ca28601a4c2bfa
eldaniell01 <amontepequem@miumg.edu.gt>
2022-11-27 01:10:18 -0600

    ejercicio 3 tipos de datos

---
+++ introducción a python/ejercicio 3/convertir tipos de datos.py
@@ -0,0 +1,6 @@
+estado = True
+print("el estado es ",type(str(estado)))
+
+my_age = float(input("la edad es "))
+print(my_age)
+print(type(my_age))
---
+++ introducción a python/ejercicio 3/tipos de datos.py
@@ -0,0 +1,36 @@
+"""
+    string
+"""
+my_name = "daniel"
+my_name = 'alvaro'
+
+print('mi nombre ', my_name)
+
+print(type(my_name))
+
+"""
+    int
+"""
+
+my_age = 25
+
+print("mi edad", my_age)
+print(type((my_age)))
+
+"""
+    boolean
+"""
+
+state = True
+state2 = False
+print("mi estado", state)
+print(type(state))
+print("mi segundo estado", state2)
+print(type(state2))
+
+"""
+    inputs
+"""
+my_age = int(input("mi edad es: "))
+print('mi edad es', my_age)
+print(type(my_age))
