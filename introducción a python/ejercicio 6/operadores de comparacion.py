7af23485fe3f002fffa91232c8b15274b32a2ffc
eldaniell01 <amontepequem@miumg.edu.gt>
2022-11-28 23:30:13 -0600

    operadores de comparacion

---
+++ introducción a python/ejercicio 6/operadores de comparacion.py
@@ -0,0 +1,43 @@
+"""
+    >
+"""
+
+print(7>2)
+
+nombre = "daniel"
+nombre2 = "lvaros"
+print(nombre>nombre2)
+print(2>7)
+print(7>7)
+
+
+"""
+    <
+"""
+print(5 <6)
+print(6<5)
+print(5<5)
+
+
+"""
+    >=
+"""
+print(2>=1)
+print(2>=3)
+print(2>=2)
+
+
+"""
+    <=
+"""
+print(1<=2)
+print(2<=1)
+print(2<=2)
+
+"""
+    == estrictamente igual
+"""
+
+print(6==6)
+print(5==2)
+print("2"==2)
