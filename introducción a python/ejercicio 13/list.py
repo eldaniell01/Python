afc4db5978ed26f7681f992d5b19dd11e489db26
eldaniell01 <amontepequem@miumg.edu.gt>
2022-12-11 21:32:58 -0600

    listas

---
+++ introducción a python/ejercicio 13/list.py
@@ -0,0 +1,18 @@
+numbers = [1, 2, 3, 4]
+
+print(numbers)
+print(type(numbers))
+
+tarea = ['hacer limpieza', 'jugar videogames']
+print(tarea)
+
+types = [1, True, 'hola']
+print(types)
+
+print(numbers[0])
+print(tarea[0])
+
+tarea[1] = 'hola me muero'
+print(tarea)
+
+print(1 in types)
