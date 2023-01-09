4c69d961a81e15ae0e9c3b23d27f7bb005de68ce
eldaniell01 <amontepequem@miumg.edu.gt>
2023-01-08 23:15:52 -0600

    inputs

--- interfaces graficas para usuarios/exercise 4/inputs.py
+++ interfaces graficas para usuarios/exercise 4/inputs.py
@@ -2,4 +2,13 @@ from tkinter import *
 
 root = Tk()
 
+input = Entry(root, )
+input.pack()
+def sumar():
+    n = input.get()
+    lab = Label(root, text=input.get())
+    print(type(n))
+    lab.pack()
+mybutton = Button(root, text='sumar', command=sumar)
+mybutton.pack()
 root.mainloop() 
